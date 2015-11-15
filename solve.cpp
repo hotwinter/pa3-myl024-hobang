0e3ddc10b5be6fb613c90c7a0c2163a4467ed1bd
Myron Liu <myronyliu@gmail.com>
2015-11-15 15:30:43 -0800

    reformatted the indenting in solve.cpp for convenience

--- solve.cpp
+++ solve.cpp
@@ -34,141 +34,164 @@ __attribute__((optimize("no-tree-vectorize")))
 // of each element, normalizing by dividing by the number of points
 // and then taking the sequare root of the result
 //
-double L2Norm(double sumSq){
-    double l2norm = sumSq /  (double) ((cb.m)*(cb.n));
-    l2norm = sqrt(l2norm);
-    return l2norm;
+double L2Norm(double sumSq)
+{
+	double l2norm = sumSq /  (double) ((cb.m)*(cb.n));
+	l2norm = sqrt(l2norm);
+	return l2norm;
 }
 
-int solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Plotter *plotter){
 
- // Simulated time is different from the integer timestep number
- double t = 0.0;
-
- double *E = *_E, *E_prev = *_E_prev;
- double *R_tmp = R;
- double *E_tmp = *_E;
- double *E_prev_tmp = *_E_prev;
- int niter;
- int m = cb.m, n=cb.n;
- int innerBlockRowStartIndex = (n+2)+1;
- int innerBlockRowEndIndex = (((m+2)*(n+2) - 1) - (n)) - (n+2);
+//MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
 
 
- // We continue to sweep over the mesh until the simulation has reached
- // the desired number of iterations
-  for (niter = 0; niter < cb.niters; niter++){
-  
-      if  (cb.debug && (niter==0)){
-	  double mx;
-          double sumSq;
-	  stats(E_prev,m,n,&mx,&sumSq);
-          double l2norm = L2Norm(sumSq);
-	  repNorms(l2norm,mx,dt,m,n,niter, cb.stats_freq);
-	  if (cb.plot_freq)
-	      plotter->updatePlot(E,  niter, m+1, n+1);
-      }
+int solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Plotter *plotter)
+{
+	// Simulated time is different from the integer timestep number
+	double t = 0.0;
+	
+	double *E = *_E, *E_prev = *_E_prev;
+	double *R_tmp = R;
+	double *E_tmp = *_E;
+	double *E_prev_tmp = *_E_prev;
+	int niter;
+	int m = cb.m, n=cb.n;
+	int innerBlockRowStartIndex = (n+2)+1;
+	int innerBlockRowEndIndex = (((m+2)*(n+2) - 1) - (n)) - (n+2);
 
-   /* 
-    * Copy data from boundary of the computational box to the
-    * padding region, set up for differencing computational box's boundary
-    *
-    * These are physical boundary conditions, and are not to be confused
-    * with ghost cells that we would use in an MPI implementation
-    *
-    * The reason why we copy boundary conditions is to avoid
-    * computing single sided differences at the boundaries
-    * which increase the running time of solve()
-    *
-    */
-    
-    // 4 FOR LOOPS set up the padding needed for the boundary conditions
-    int i,j;
 
-    // Fills in the TOP Ghost Cells
-    for (i = 0; i < (n+2); i++) {
-        E_prev[i] = E_prev[i + (n+2)*2];
-    }
+	// We continue to sweep over the mesh until the simulation has reached
+	// the desired number of iterations
+	for (niter = 0; niter < cb.niters; niter++)
+	{
+		if (cb.debug && (niter==0))
+		{
+			double mx;
+			double sumSq;
+			stats(E_prev,m,n,&mx,&sumSq);
+			double l2norm = L2Norm(sumSq);
+			repNorms(l2norm,mx,dt,m,n,niter, cb.stats_freq);
+			if (cb.plot_freq)
+			{
+				plotter->updatePlot(E,  niter, m+1, n+1);
+			}
+		}
 
-    // Fills in the RIGHT Ghost Cells
-    for (i = (n+1); i < (m+2)*(n+2); i+=(n+2)) {
-        E_prev[i] = E_prev[i-2];
-    }
+		/* 
+		 * Copy data from boundary of the computational box to the
+		 * padding region, set up for differencing computational box's boundary
+		 *
+		 * These are physical boundary conditions, and are not to be confused
+		 * with ghost cells that we would use in an MPI implementation
+		 *
+		 * The reason why we copy boundary conditions is to avoid
+		 * computing single sided differences at the boundaries
+		 * which increase the running time of solve()
+		 *
+		 */
+ 
+		// 4 FOR LOOPS set up the padding needed for the boundary conditions
+		int i,j;
 
-    // Fills in the LEFT Ghost Cells
-    for (i = 0; i < (m+2)*(n+2); i+=(n+2)) {
-        E_prev[i] = E_prev[i+2];
-    }	
+		// Fills in the TOP Ghost Cells
+		for (i = 0; i < (n+2); i++)
+		{
+			E_prev[i] = E_prev[i + (n+2)*2];
+		}
 
-    // Fills in the BOTTOM Ghost Cells
-    for (i = ((m+2)*(n+2)-(n+2)); i < (m+2)*(n+2); i++) {
-        E_prev[i] = E_prev[i - (n+2)*2];
-    }
+		// Fills in the RIGHT Ghost Cells
+		for (i = (n+1); i < (m+2)*(n+2); i+=(n+2))
+		{
+			E_prev[i] = E_prev[i-2];
+		}
 
-//////////////////////////////////////////////////////////////////////////////
+		// Fills in the LEFT Ghost Cells
+		for (i = 0; i < (m+2)*(n+2); i+=(n+2))
+		{
+			E_prev[i] = E_prev[i+2];
+		}
 
-// #define FUSED 1
+		// Fills in the BOTTOM Ghost Cells
+		for (i = ((m+2)*(n+2)-(n+2)); i < (m+2)*(n+2); i++)
+		{
+			E_prev[i] = E_prev[i - (n+2)*2];
+		}
 
-#ifdef FUSED
-    // Solve for the excitation, a PDE
-    for(j = innerBlockRowStartIndex; j <= innerBlockRowEndIndex; j+=(n+2)) {
-        E_tmp = E + j;
-	E_prev_tmp = E_prev + j;
-        R_tmp = R + j;
-	for(i = 0; i < n; i++) {
-	    E_tmp[i] = E_prev_tmp[i]+alpha*(E_prev_tmp[i+1]+E_prev_tmp[i-1]-4*E_prev_tmp[i]+E_prev_tmp[i+(n+2)]+E_prev_tmp[i-(n+2)]);
-            E_tmp[i] += -dt*(kk*E_tmp[i]*(E_tmp[i]-a)*(E_tmp[i]-1)+E_tmp[i]*R_tmp[i]);
-            R_tmp[i] += dt*(epsilon+M1* R_tmp[i]/( E_tmp[i]+M2))*(-R_tmp[i]-kk*E_tmp[i]*(E_tmp[i]-b-1));
-        }
-    }
-#else
-    // Solve for the excitation, a PDE
-    for(j = innerBlockRowStartIndex; j <= innerBlockRowEndIndex; j+=(n+2)) {
-        E_tmp = E + j;
-            E_prev_tmp = E_prev + j;
-            for(i = 0; i < n; i++) {
-                E_tmp[i] = E_prev_tmp[i]+alpha*(E_prev_tmp[i+1]+E_prev_tmp[i-1]-4*E_prev_tmp[i]+E_prev_tmp[i+(n+2)]+E_prev_tmp[i-(n+2)]);
-            }
-    }
+		//////////////////////////////////////////////////////////////////////////////
 
-    /* 
-     * Solve the ODE, advancing excitation and recovery variables
-     *     to the next timtestep
-     */
+		// #define FUSED 1
 
-    for(j = innerBlockRowStartIndex; j <= innerBlockRowEndIndex; j+=(n+2)) {
-        E_tmp = E + j;
-        R_tmp = R + j;
-        for(i = 0; i < n; i++) {
-            E_tmp[i] += -dt*(kk*E_tmp[i]*(E_tmp[i]-a)*(E_tmp[i]-1)+E_tmp[i]*R_tmp[i]);
-            R_tmp[i] += dt*(epsilon+M1* R_tmp[i]/( E_tmp[i]+M2))*(-R_tmp[i]-kk*E_tmp[i]*(E_tmp[i]-b-1));
-        }
-    }
-#endif
-     /////////////////////////////////////////////////////////////////////////////////
+	#ifdef FUSED
+		// Solve for the excitation, a PDE
+		for (j = innerBlockRowStartIndex; j <= innerBlockRowEndIndex; j+=(n+2))
+		{
+			E_tmp = E + j;
+			E_prev_tmp = E_prev + j;
+			R_tmp = R + j;
 
-   if ((cb.stats_freq) && (niter !=0)){
-     double mx;
-     double sumSq;
-     stats(E_prev,m,n,&mx,&sumSq);
-     double l2norm = L2Norm(sumSq);
-     repNorms(l2norm,mx,dt,m,n,niter, cb.stats_freq);
-   }
+			for (i = 0; i < n; i++)
+			{
+				E_tmp[i] = E_prev_tmp[i]+alpha*(E_prev_tmp[i+1]+E_prev_tmp[i-1]-4*E_prev_tmp[i]+E_prev_tmp[i+(n+2)]+E_prev_tmp[i-(n+2)]);
+				E_tmp[i] += -dt*(kk*E_tmp[i]*(E_tmp[i]-a)*(E_tmp[i]-1)+E_tmp[i]*R_tmp[i]);
+				R_tmp[i] += dt*(epsilon+M1* R_tmp[i]/( E_tmp[i]+M2))*(-R_tmp[i]-kk*E_tmp[i]*(E_tmp[i]-b-1));
+			}
+		}
+	#else
+		// Solve for the excitation, a PDE
+		for (j = innerBlockRowStartIndex; j <= innerBlockRowEndIndex; j+=(n+2))
+		{
+			E_tmp = E + j;
+			E_prev_tmp = E_prev + j;
 
-   if (cb.plot_freq){
-          if (!(niter % cb.plot_freq)){
-	    plotter->updatePlot(E,  niter, m, n);
-        }
-    }
+			for (i = 0; i < n; i++)
+			{
+				E_tmp[i] = E_prev_tmp[i]+alpha*(E_prev_tmp[i+1]+E_prev_tmp[i-1]-4*E_prev_tmp[i]+E_prev_tmp[i+(n+2)]+E_prev_tmp[i-(n+2)]);
+			}
+		}
 
-   // Swap current and previous meshes
-   double *tmp = E; E = E_prev; E_prev = tmp;
+		/* 
+		 * Solve the ODE, advancing excitation and recovery variables
+		 *     to the next timtestep
+		 */
 
- } //end of 'niter' loop at the beginning
+		for (j = innerBlockRowStartIndex; j <= innerBlockRowEndIndex; j+=(n+2))
+		{
+			E_tmp = E + j;
+			R_tmp = R + j;
+			for(i = 0; i < n; i++)
+			{
+				E_tmp[i] += -dt*(kk*E_tmp[i]*(E_tmp[i]-a)*(E_tmp[i]-1)+E_tmp[i]*R_tmp[i]);
+				R_tmp[i] += dt*(epsilon+M1* R_tmp[i]/( E_tmp[i]+M2))*(-R_tmp[i]-kk*E_tmp[i]*(E_tmp[i]-b-1));
+			}
+		}
+	#endif
+		/////////////////////////////////////////////////////////////////////////////////
 
-  // Swap pointers so we can re-use the arrays
-  *_E = E;
-  *_E_prev = E_prev;
+		if ((cb.stats_freq) && (niter !=0))
+		{
+			double mx;
+			double sumSq;
+			stats(E_prev,m,n,&mx,&sumSq);
+			double l2norm = L2Norm(sumSq);
+			repNorms(l2norm,mx,dt,m,n,niter, cb.stats_freq);
+		}
 
-  return niter;
+		if (cb.plot_freq)
+		{
+			if (!(niter % cb.plot_freq))
+			{
+				plotter->updatePlot(E,  niter, m, n);
+			}
+		}
+
+		// Swap current and previous meshes
+		double *tmp = E; E = E_prev; E_prev = tmp;
+
+	} //end of 'niter' loop at the beginning
+
+	// Swap pointers so we can re-use the arrays
+	*_E = E;
+	*_E_prev = E_prev;
+	
+	return niter;
 }
