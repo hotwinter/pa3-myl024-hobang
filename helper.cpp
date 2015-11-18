2eba847122f55d329fffb246808c8d9adda79d3d
Myron Liu <myronyliu@gmail.com>
2015-11-18 12:45:04 -0800

    Modified alloc1D() and init() in helper.cpp to split up the problem among processes. To test, comment out the restriction on the mimimum problem size of 26 in apf.cpp; printMat() will then print out the initial subproblem arrays, including ghost cells.

--- helper.cpp
+++ helper.cpp
@@ -6,13 +6,22 @@
 
 #include <iostream>
 #include <assert.h>
-// Needed for memalign
 #include <malloc.h>
+#include "cblock.h"
 #include <mpi.h>
-
+#include <string.h>
 using namespace std;
 
+extern control_block cb;
+
+int my_rank = 0;
+int my_n = 0; // the number of columns in the subproblem
+int my_m = 0; // ............. rows    .................
+
 void printMat(const char mesg[], double *E, int m, int n);
+
+inline int min(int a, int b) { if (a < b) return a; else return b; }
+inline int max(int a, int b) { if (a > b) return a; else return b; }
 
 //
 // Initialization
@@ -22,91 +31,128 @@ void printMat(const char mesg[], double *E, int m, int n);
 // These coordinates are in world (global) coordinate and must
 // be mapped to appropriate local indices when parallelizing the code
 //
-void init (double *E,double *E_prev,double *R,int m,int n)
+void init (double *E, double *E_prev, double *R, int m, int n)
 {
-	int i;
+	// By now, global variables my_rank, my_m, my_n have already been set
 
-	for (i=0; i < (m+2)*(n+2); i++)
+	int px = cb.px;
+	int py = cb.py;
+
+	int nMin = n / cb.px;
+	int mMin = m / cb.py;
+
+	// Number of block-rows/cols that are extended by one
+	int rx = n % cb.px;
+	int ry = m % cb.py;
+
+	// Position of the process in the grid of processes
+	int iP = my_rank / px;
+	int jP = my_rank % px;
+
+	///////////////////////////////////////////////////////////////////////////////////////
+	//// Initialize R (R's ghosts cells are arbitrary since we don't use them anyways) ////
+	///////////////////////////////////////////////////////////////////////////////////////
+
+	int iMin = iP*mMin + min(iP, ry); // GLOBAL index of the first row of the "computational" block (ignoring ghost cells)
+	int di = (m + 1)/2 - iMin; // Distance from iMin to the last row of 0.0s
+
+	for (int i = my_n + 2; i < (di + 1)*(my_n + 2); ++i)
 	{
-		E_prev[i] = R[i] = 0;
+		R[i] = 0.0;
 	}
-
-	for (i = (n+2); i < (m+1)*(n+2); i++)
+	for (int i = (di + 1)*(my_n + 2); i< (my_m + 1)*(my_n + 2); ++i)
 	{
-		int colIndex = i % (n+2);		// gives the base index (first row's) of the current index
-
-		// Need to compute (n+1)/2 rather than n/2 to work with odd numbers
-		if (colIndex == 0 || colIndex == (n+1) || colIndex < ((n+1)/2+1))
-		{
-			continue;
-		}
-		E_prev[i] = 1.0;
-	}
-
-	for (i = 0; i < (m+2)*(n+2); i++)
-	{
-		int rowIndex = i / (n+2);		// gives the current row number in 2D array representation
-		int colIndex = i % (n+2);		// gives the base index (first row's) of the current index
-
-		// Need to compute (m+1)/2 rather than m/2 to work with odd numbers
-		if (colIndex == 0 || colIndex == (n+1) || rowIndex < ((m+1)/2+1))
-		{
-			continue;
-		}
 		R[i] = 1.0;
 	}
+
+	///////////////////////////////////////////////////////////////////////////////
+	//// Initialize E_prev (E_prev's ghost cells are initialized to 0)         ////
+	//// Msg passing will fill in interior ghost cells with appropriate values ////
+	///////////////////////////////////////////////////////////////////////////////
+
+	int jMin = jP*nMin + min(jP, rx);
+	int dj = (n + 1)/2 - jMin;
+
+	for (int i = 0; i < (my_m + 2)*(my_n + 2); ++i)
+	{
+		int rowIndex = i / (my_n + 2);
+		int colIndex = i % (my_n + 2);
+
+		if (colIndex == 0 || colIndex == my_n + 1 || colIndex <= dj ||
+			rowIndex == 0 || rowIndex == my_m + 1)
+		{
+			E_prev[i] = 0.0;
+		}
+		else
+		{
+			E_prev[i] = 1.0;
+		}
+	}
+
 	// We only print the meshes if they are small enough
-	printMat("E_prev",E_prev,m,n);
-	printMat("R",R,m,n);
+	printf("\n\nRANK %d INITIAL CONDITIONS:\n\n", my_rank);
+	printMat("E_prev", E_prev, my_m, my_n);
+	printMat("R", R, my_m, my_n);
 }
 
-double *alloc1D(int m,int n)
+// NOTE: This gets called with arguments (cb.m+2, cb.n+2) in apf.cpp
+double *alloc1D(int mPlus2,int nPlus2)
 {
-	int nx=n, ny=m;
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+	int m = mPlus2 - 2; // get the unpadded size of the global problem
+	int n = nPlus2 - 2;
+
+	my_m = m / cb.py + ((my_rank / cb.px) < (m % cb.py));
+	my_n = n / cb.px + ((my_rank % cb.px) < (n % cb.px));
+
 	double *E;
-	// Ensures that allocatdd memory is aligned on a 16 byte boundary
-	assert(E= (double*) memalign(16, sizeof(double)*nx*ny) );
+	// Ensure that allocated memory is aligned on a 16 byte boundary
+	// Pad the subproblem to accomodate E_prev's ghost cells
+	// We also pad R, although this is redundant and just for consistency
+	assert(E= (double*) memalign(16, sizeof(double)*(my_m + 2)*(my_n + 2)));
 	return(E);
 }
 
 void printMat(const char mesg[], double *E, int m, int n)
 {
-	int i;
-	if (m>8)
+	if (m > 8)
 	{
 		return;
 	}
-	printf("%s\n",mesg);
+	printf("%s\n", mesg);
 
-	for (i=0; i < (m+2)*(n+2); i++)
+	for (int i = 0; i < (m + 2)*(n + 2); ++i)
 	{
-		int rowIndex = i / (n+2);
-		int colIndex = i % (n+2);
-		if ((colIndex>0) && (colIndex<m+1))
-		{
-			if ((rowIndex > 0) && (rowIndex < n+1))
-			{
-				printf("%6.3f ", E[i]);
-			}
-		}
-		if (colIndex == m+1)
+		int rowIndex = i / (n + 2);
+		int colIndex = i % (n + 2);
+
+		printf("%6.3f ", E[i]); // For testing purposes, we also print the ghost cells
+
+//		if ((colIndex>0) && (colIndex<n+1))
+//		{
+//			if ((rowIndex > 0) && (rowIndex < m+1))
+//			{
+//				printf("%6.3f ", E[i]);
+//			}
+//		}
+		if (colIndex == n+1)
 		{
 			printf("\n");
 		}
 	}
 }
 
-enum
-{
-	LEFT = 0,
-	RIGHT,
-	TOP,
-	BOTTOM
-};
-
-int getTag(int edge)
+inline int getTag(int edge)
 {
 	int rank = 0;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	return (rank << 2) + edge;
+}
+
+inline int getRank()
+{
+	int rank = 0;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	return rank;
 }
--- solve.cpp
+++ solve.cpp
@@ -17,7 +17,10 @@
 #include "Plotting.h"
 #include "cblock.h"
 #include <emmintrin.h>
+#include <mpi.h>
 using namespace std;
+
+enum { NORTH = 0, EAST, WEST, SOUTH };
 
 void repNorms(double l2norm, double mx, double dt, int m,int n, int niter, int stats_freq);
 void stats(double *E, int m, int n, double *_mx, double *sumSq);
@@ -40,6 +43,86 @@ double L2Norm(double sumSq)
 	l2norm = sqrt(l2norm);
 	return l2norm;
 }
+
+// Either pads the matrix with boundary condiditions (if the block lies on the problem boundaries)
+// Or waits for the message that holds the needed ghost cells
+void fillGhostCells(double *E, double *E_prev)
+{
+	MPI_Status status;
+
+	int rank = 0;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	int n = cb.n;
+	int m = cb.m;
+
+	int x = rank % n;
+	int y = rank % m;
+
+	double* msg_N = new double[2*(m + n)];
+	double* msg_E = msg_N + n;
+	double* msg_W = msg_E + m;
+	double* msg_S = msg_W + m;
+	
+
+	// 4 FOR LOOPS set up the padding needed for the boundary conditions
+	int i,j;
+
+	// Fills in the WEST Ghost Cells
+	if (x == 0)
+	{
+		for (i = 0; i < (m+2)*(n+2); i+=(n+2))
+		{
+			E_prev[i] = E_prev[i+2];
+		}
+	}
+	else
+	{
+		MPI_Recv(msg_W, m, MPI_DOUBLE, rank - 1, (rank - 1) << 2 + EAST, MPI_COMM_WORLD, &status);
+	}
+
+	// Fills in the EAST Ghost Cells
+	if (x == n-1)
+	{
+		for (i = (n+1); i < (m+2)*(n+2); i+=(n+2))
+		{
+			E_prev[i] = E_prev[i-2];
+		}
+	}
+	else
+	{
+		MPI_Recv(msg_E, m, MPI_DOUBLE, rank + 1, (rank + 1) << 2 + WEST, MPI_COMM_WORLD, &status);
+	}
+
+	// Fills in the NORTH Ghost Cells
+	if (y == 0)
+	{
+		for (i = 0; i < (n+2); i++)
+		{
+			E_prev[i] = E_prev[i + (n+2)*2];
+		}
+	}
+	else
+	{
+		//MPI_Recv(msg_N, n, MPI_DOUBLE, rank + )
+	}
+
+	// Fills in the SOUTH Ghost Cells
+	if (y == n-1)
+	{
+		for (i = ((m+2)*(n+2)-(n+2)); i < (m+2)*(n+2); i++)
+		{
+			E_prev[i] = E_prev[i - (n+2)*2];
+		}
+	}
+	else
+	{
+
+	}
+
+	delete[] msg_N;
+}
+
 
 int solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Plotter *plotter)
 {
@@ -67,6 +150,7 @@ int solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Plo
 			stats(E_prev,m,n,&mx,&sumSq);
 			double l2norm = L2Norm(sumSq);
 			repNorms(l2norm,mx,dt,m,n,niter, cb.stats_freq);
+
 			if (cb.plot_freq)
 			{
 				plotter->updatePlot(E,  niter, m+1, n+1);
@@ -154,7 +238,7 @@ int solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Plo
 		{
 			E_tmp = E + j;
 			R_tmp = R + j;
-			for(i = 0; i < n; i++)
+			for (i = 0; i < n; i++)
 			{
 				E_tmp[i] += -dt*(kk*E_tmp[i]*(E_tmp[i]-a)*(E_tmp[i]-1)+E_tmp[i]*R_tmp[i]);
 				R_tmp[i] += dt*(epsilon+M1* R_tmp[i]/( E_tmp[i]+M2))*(-R_tmp[i]-kk*E_tmp[i]*(E_tmp[i]-b-1));
