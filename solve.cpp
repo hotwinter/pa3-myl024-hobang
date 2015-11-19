e67d4805a74800b60536f52e773481cfa67cfe95
Myron Liu <myronyliu@gmail.com>
2015-11-19 15:44:36 -0800

    changed to nonblocking MPI_Isend and MPI_Irecv with MPI_Waitall barrier

--- solve.cpp
+++ solve.cpp
@@ -62,99 +62,118 @@ enum { NORTH = 0, EAST, WEST, SOUTH };
 // - For INTERIOR process boundaries, we send the boundary computational
 //   cells to the neighboring process, which also produces the data for
 //   the boundary ghost cells. 
+
+void send(double *E_prev)
+{
+
+}
+
+void recv(double *E_prev)
+{
+
+}
+
 void communicate(double *E_prev)
 {
 	int i, j;
-	MPI_Status status;
+	MPI_Request requests[8];
+	MPI_Status statuses[4];
 
-	double* in_N= new double[2*(my_m + my_n)]; // allocate some space for the received messages
-	double* in_E = in_N + my_n;
-	double* in_W = in_E + my_m;
-	double* in_S = in_W + my_m;
+	double* in_W = new double[2*my_m]; // allocate some space for the received messages
+	double* in_E = in_W + my_m;
 
-	double* out_N = new double[2*(my_m + my_n)]; // allocate some space for the sent messages
-	double* out_E = out_N + my_n;
-	double* out_W = out_E + my_m;
-	double* out_S = out_W + my_m;
-
-	// Send the WEST boundary & fill the WEST ghost cells
-	if (my_pj == 0) // ... then the process lies at the WEST boundary of the GLOBAL problem
-	{
-		for (i = my_n + 2; i < (my_m + 1)*(my_n + 2); i += my_n + 2)
-		{
-			E_prev[i] = E_prev[i + 2];
-		}
-	}
-	else // ... the process's WEST boundary lies in the interior of the GLOBAL problem 
-	{
-		MPI_Send(out_W, my_m, MPI_DOUBLE, my_rank, WEST, MPI_COMM_WORLD);
-		MPI_Recv(in_W, my_m, MPI_DOUBLE, my_rank - 1, EAST, MPI_COMM_WORLD, &status);
-
-		for (i = my_n + 2, j = 0; j < my_m; i += my_n + 2, ++j) 		
-		{
-			E_prev[i] = in_W[j];
-		}
-	}
-
-	// Send the EAST boundary & fill the EAST ghost cells
-	if (my_pj == cb.px - 1)
-	{
-		for (i = (my_n + 1) + (my_n + 2); i < (my_n + 1) + (my_m + 1)*(my_n + 2); i += my_n + 2)
-		{
-			E_prev[i] = E_prev[i - 2];
-		}
-	}
-	else
-	{
-		MPI_Send(out_E, my_m, MPI_DOUBLE, my_rank, EAST, MPI_COMM_WORLD);
-		MPI_Recv(in_E, my_m, MPI_DOUBLE, my_rank + 1, WEST, MPI_COMM_WORLD, &status);
-
-		for (i = (my_n + 1) + (my_n + 2), j = 0; j < my_m; i += my_n + 2, ++j)
-		{
-			E_prev[i] = in_E[j];
-		}
-	}
+	double* out_W = new double[2*my_m]; // allocate some space for the sent messages
+	double* out_E = out_W + my_m;
 
 	// Send the NORTH boundary & fill the NORTH ghost cells
 	if (my_pi == 0)
 	{
 		for (i = 1; i < my_n + 1; ++i)
 		{
+			printf("N  %d  %d\n", i, i+2*(my_n + 2));
 			E_prev[i] = E_prev[i + 2*(my_n + 2)];
 		}
 	}
 	else
 	{
-		MPI_Send(out_N, my_n, MPI_DOUBLE, my_rank, NORTH, MPI_COMM_WORLD);
-		MPI_Recv(in_N, my_n, MPI_DOUBLE, my_rank + cb.px, SOUTH, MPI_COMM_WORLD, &status);
-
-		for (j = 0; j < my_n; ++j)
-		{
-			E_prev[j + 1] = in_N[j];
-		}
+		MPI_Irecv(&E_prev[1], my_n, MPI_DOUBLE, my_rank - cb.px, SOUTH, MPI_COMM_WORLD, requests + 0);
+		MPI_Isend(&E_prev[my_n + 3], my_n, MPI_DOUBLE, my_rank, NORTH, MPI_COMM_WORLD, requests + 4);
 	}
 
 	// Send the SOUTH boundary & fill the SOUTH ghost cells
 	if (my_pi == cb.py - 1)
 	{
-		for (i = (my_m + 2)*(my_n + 2)-(my_n + 1); i < (my_m+2)*(my_n+2) - 1; ++i)
+		for (i = (my_m + 1)*(my_n + 2) + 1; i < (my_m + 2)*(my_n + 2) - 1; ++i)
 		{
 			E_prev[i] = E_prev[i - 2*(my_n + 2)];
 		}
 	}
 	else
 	{
-		MPI_Send(out_S, my_n, MPI_DOUBLE, my_rank, SOUTH, MPI_COMM_WORLD);
-		MPI_Recv(in_S, my_n, MPI_DOUBLE, my_rank - cb.px, NORTH, MPI_COMM_WORLD, &status);
+		printf("S\n");
+		MPI_Irecv(&E_prev[(my_m + 1)*(my_n + 2) + 1], my_n, MPI_DOUBLE, my_rank - cb.px, NORTH, MPI_COMM_WORLD, requests + 1);
+		MPI_Isend(&E_prev[my_m*(my_n + 2) + 1], my_n, MPI_DOUBLE, my_rank, SOUTH, MPI_COMM_WORLD, requests + 5);
+		printf("SS\n");
+	}
 
-		for (j = 0; j < my_n; ++j)
+	// Send the WEST boundary & fill the WEST ghost cells
+	if (my_pj == 0)
+	{
+		for (i = my_n + 2; i < (my_m + 1)*(my_n + 2); i += my_n + 2)
 		{
-			E_prev[j + (my_m + 2)*(my_n + 2)-(my_n + 1)] = in_S[j];
+			E_prev[i] = E_prev[i + 2];
+		}
+	}
+	else
+	{
+		for (i = my_n + 3, j = 0; j < my_m; i += my_n + 2, ++j)
+		{
+			printf("W  %d  %d\n", i, j);
+			out_W[j] = E_prev[i];
+		}
+		MPI_Irecv(in_W, my_m, MPI_DOUBLE, my_rank - 1, EAST, MPI_COMM_WORLD, requests + 2);
+		MPI_Isend(&E_prev[my_n + 3], my_m, MPI_DOUBLE, my_rank, WEST, MPI_COMM_WORLD, requests + 6);
+	}
+
+	// Send the EAST boundary & fill the EAST ghost cells
+	if (my_pj == cb.px - 1)
+	{
+		for (i = (my_n + 1) + 1*(my_n + 2); i < (my_n + 1) + (my_m + 1)*(my_n + 2); i += my_n + 2)
+		{
+			//printf("%d  %d  %d\n", my_m, my_n, i);
+			E_prev[i] = E_prev[i - 2];
+		}
+	}
+	else
+	{
+		for (i = (my_n + 1) + (my_n + 2), j = 0; j < my_m; i += my_n + 2, ++j)
+		{
+			out_E[j] = E_prev[i];
+		}
+		MPI_Irecv(in_E, my_m, MPI_DOUBLE, my_rank + 1, WEST, MPI_COMM_WORLD, requests + 3);
+		MPI_Isend(out_E, my_m, MPI_DOUBLE, my_rank, EAST, MPI_COMM_WORLD, requests + 7);
+	}
+
+	MPI_Waitall(4, requests, statuses);
+
+	if (my_pj != 0)
+	{
+		for (i = my_n + 2, j = 0; j < my_m; i += my_n + 2, ++j) 		
+		{
+			E_prev[i] = in_W[j];
 		}
 	}
 
-	delete[] in_N;
-	delete[] out_N;
+	if (my_pj != cb.px - 1)
+	{
+		for (i = (my_n + 1) + (my_n + 2), j = 0; j < my_m; i += my_n + 2, ++j)
+		{
+			E_prev[i] = in_E[j];
+		}
+	}
+
+	delete[] in_W;
+	delete[] out_W;
 }
 
 
@@ -169,8 +188,8 @@ int solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Plo
 	double *E_prev_tmp = *_E_prev;
 	int niter;
 	int m = cb.m, n=cb.n;
-	int innerBlockRowStartIndex = (n+2)+1;
-	int innerBlockRowEndIndex = (((m+2)*(n+2) - 1) - (n)) - (n+2);
+	int innerBlockRowStartIndex = (my_n+2)+1;
+	int innerBlockRowEndIndex = (((my_m+2)*(my_n+2) - 1) - (my_n)) - (my_n+2);
 
 
 	// We continue to sweep over the mesh until the simulation has reached
@@ -187,13 +206,13 @@ int solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Plo
 
 			if (cb.plot_freq)
 			{
-				plotter->updatePlot(E,  niter, m+1, n+1);
+				//plotter->updatePlot(E,  niter, m+1, n+1);
 			}
 		}
 
 		int i, j;
 
-		communicate(E_prev);
+//		communicate(E_prev);
 
 		//////////////////////////////////////////////////////////////////////////////
 
@@ -201,29 +220,29 @@ int solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Plo
 
 	#ifdef FUSED
 		// Solve for the excitation, a PDE
-		for (j = innerBlockRowStartIndex; j <= innerBlockRowEndIndex; j+=(n+2))
+		for (j = innerBlockRowStartIndex; j <= innerBlockRowEndIndex; j+= my_n + 2)
 		{
 			E_tmp = E + j;
 			E_prev_tmp = E_prev + j;
 			R_tmp = R + j;
 
-			for (i = 0; i < n; i++)
+			for (i = 0; i < my_n; i++)
 			{
-				E_tmp[i] = E_prev_tmp[i]+alpha*(E_prev_tmp[i+1]+E_prev_tmp[i-1]-4*E_prev_tmp[i]+E_prev_tmp[i+(n+2)]+E_prev_tmp[i-(n+2)]);
+				E_tmp[i] = E_prev_tmp[i]+alpha*(E_prev_tmp[i+1]+E_prev_tmp[i-1]-4*E_prev_tmp[i]+E_prev_tmp[i+(my_n+2)]+E_prev_tmp[i-(my_n+2)]);
 				E_tmp[i] += -dt*(kk*E_tmp[i]*(E_tmp[i]-a)*(E_tmp[i]-1)+E_tmp[i]*R_tmp[i]);
 				R_tmp[i] += dt*(epsilon+M1* R_tmp[i]/( E_tmp[i]+M2))*(-R_tmp[i]-kk*E_tmp[i]*(E_tmp[i]-b-1));
 			}
 		}
 	#else
 		// Solve for the excitation, a PDE
-		for (j = innerBlockRowStartIndex; j <= innerBlockRowEndIndex; j+=(n+2))
+		for (j = innerBlockRowStartIndex; j <= innerBlockRowEndIndex; j+=my_n+2)
 		{
 			E_tmp = E + j;
 			E_prev_tmp = E_prev + j;
 
-			for (i = 0; i < n; i++)
+			for (i = 0; i < my_n; i++)
 			{
-				E_tmp[i] = E_prev_tmp[i]+alpha*(E_prev_tmp[i+1]+E_prev_tmp[i-1]-4*E_prev_tmp[i]+E_prev_tmp[i+(n+2)]+E_prev_tmp[i-(n+2)]);
+				E_tmp[i] = E_prev_tmp[i]+alpha*(E_prev_tmp[i+1]+E_prev_tmp[i-1]-4*E_prev_tmp[i]+E_prev_tmp[i+(my_n+2)]+E_prev_tmp[i-(my_n+2)]);
 			}
 		}
 
@@ -232,11 +251,11 @@ int solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Plo
 		 *     to the next timtestep
 		 */
 
-		for (j = innerBlockRowStartIndex; j <= innerBlockRowEndIndex; j+=(n+2))
+		for (j = innerBlockRowStartIndex; j <= innerBlockRowEndIndex; j+=(my_n+2))
 		{
 			E_tmp = E + j;
 			R_tmp = R + j;
-			for (i = 0; i < n; i++)
+			for (i = 0; i < my_n; i++)
 			{
 				E_tmp[i] += -dt*(kk*E_tmp[i]*(E_tmp[i]-a)*(E_tmp[i]-1)+E_tmp[i]*R_tmp[i]);
 				R_tmp[i] += dt*(epsilon+M1* R_tmp[i]/( E_tmp[i]+M2))*(-R_tmp[i]-kk*E_tmp[i]*(E_tmp[i]-b-1));
@@ -249,16 +268,16 @@ int solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Plo
 		{
 			double mx;
 			double sumSq;
-			stats(E_prev,m,n,&mx,&sumSq);
+			//stats(E_prev,m,n,&mx,&sumSq);
 			double l2norm = L2Norm(sumSq);
-			repNorms(l2norm,mx,dt,m,n,niter, cb.stats_freq);
+			//repNorms(l2norm,mx,dt,m,n,niter, cb.stats_freq);
 		}
 
 		if (cb.plot_freq)
 		{
 			if (!(niter % cb.plot_freq))
 			{
-				plotter->updatePlot(E,  niter, m, n);
+				//plotter->updatePlot(E,  niter, m, n);
 			}
 		}
 
